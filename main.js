ca0025d40a4f48b1809468942ab19080f171a3b4
“philberdecio” <philberdecio@gmail.com>
2022-08-11 15:01:27 -0500

    revised and expanded functionality

--- main.js
+++ main.js
@@ -36,50 +36,56 @@ const questions = [question1, question2];
 
 let score = 0;
 let questions_asked = 0;
-document.querySelector("button").addEventListener("click", startGame);
+let currentQuestion = questions.pop();
 
-/*function startGame() {
-    let nextButton = document.querySelector("#button-div");
-    nextButton.innerHTML = "";
-}*/
+document.querySelector("#start-button").addEventListener("click", nextQuestion);
+
+function checkNext() {
+    document.querySelector("#button-div").innerHTML = "";
+    if (questions.length > 0) {
+        currentQuestion = questions.pop();
+        nextQuestion()
+    } else {
+        endGame();
+    }
+}
 
 
-function startGame() {
+function nextQuestion() {
     let answerResult = document.querySelector("#result-div");
-    //let nextQuestion = document.querySelector("#button-div")
     answerResult.innerHTML = `<p id = "display-result"></p>`;
-    for ( let i = 0; i < questions.length; i++) {
-        let currentQuestion = questions[i];
-        // replace with random selection from questions array
-        currentQuestion.asked = true;
-        let currentAnswer = currentQuestion.answerDisplay;
-        renderQuestion(currentQuestion)
-        let radios = document.querySelectorAll("input")
-        let submitButton = document.querySelector("#toSubmit")
-        submitButton.addEventListener("click",(e) => {
-            e.preventDefault();
-            for (let i = 0; i < radios.length; i++) {
-                console.log(`length: ${radios.length}`)
-                if (radios[i].checked === true) {
-                    if (radios[i].value === currentQuestion.answer) {
+    document.querySelector("#button-div").innerHTML= ""
+    currentQuestion.asked = true;
+    let currentAnswer = currentQuestion.answerDisplay;
+    renderQuestion(currentQuestion)
+    let radios = document.querySelectorAll("input")
+    let submitButton = document.querySelector("#toSubmit")
+    submitButton.addEventListener("click",(e) => {
+        e.preventDefault();
+        for (let i = 0; i < radios.length; i++) {
+            console.log(`length: ${radios.length}`)
+            if (radios[i].checked === true) {
+                if (radios[i].value === currentQuestion.answer) {
                         document.querySelector("#display-result").innerText = "Correct!"
                         score++;
+                        document.querySelector("#button-div").innerHTML = `<button id ="next-button" type="button">Next</button>`
+                        document.querySelector("#next-button").addEventListener("click", checkNext);
                         
                     } else if (radios[i] !== currentQuestion.answer) {
                         document.querySelector("#display-result").innerText = `Incorrect! The correct answer is ${currentAnswer}`;
+                        document.querySelector("#button-div").innerHTML = `<button id ="next-button" type="button">Next</button>`
+                        document.querySelector("#next-button").addEventListener("click", checkNext);
+                        
                         
                     }
                 }   
             }
         })
-        console.log("exit loop")
-    }
-    console.log("finished")
 }
 
 function renderQuestion (questionObject) {
     let questionText = document.querySelector(".q-text");
-    let answers = document.querySelector(".answers-div");
+    let answers = document.querySelector("#answers-div");
     questionText.innerText = questionObject.question;
     answers.innerHTML = "<form id='answers-form'></form>";
     let answersForm = document.querySelector("#answers-form");
@@ -93,20 +99,28 @@ function renderQuestion (questionObject) {
     <label class = "labels" id="label-a" for="A">${answer1}</label><input type="radio" id="choice-B" name="choices" value="B"></input>
     <label class = "labels" id = "label-b" for="B">${answer2}</label><input type="radio" id="choice-C" name="choices" value="C"></input>
     <label class = "labels" id="label-c" for="C">${answer3}</label><input type="radio" id="choice-D" name="choices" value="D"></input>
-    <label class = "labels" id="label-d" for="D">${answer4}</label>"<button id="toSubmit" type="submit" form="answers-form">Submit</button>`
+    <label class = "labels" id="label-d" for="D">${answer4}</label><button id="toSubmit" type="submit" form="answers-form">Submit</button>`
 }
 
-/*function endGame () {
-    finishButton = document.querySelector("#toSubmit");
-    finishButton.innerHTML(Finish);
-    inishButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        let displayResults = document.querySelector(".q-text");
-        if (score < 5) {
-            displayResults.innerHTML = `You scored ${score} out of 10. Time to study up on your Dune lore!`;
-        } else if (score < 8) {
-            displayResults.innerHTML = `You scored ${score} out of 10. You really know your way around the Imperium!`
-        } else {
-            displayResults.innerHTML = `You scored ${score} out of 10. Nice job! You're practically a mentat!`
-        }
-    })}*/
+function endGame () {
+    let displayResults = document.querySelector(".q-text");
+    document.querySelector("#answers-div").innerHTML = `<p class="starter-text">Want to try again? Click the button below to get started!</p>`
+    document.querySelector("#display-result").innerText = ""
+    document.querySelector("#button-div").innerHTML = `<button id ="start-button" type="button">start</button>`
+    document.querySelector("#start-button").addEventListener("click", newGame);
+    if (score < 5) {
+        displayResults.innerHTML = `You scored ${score} out of 10. Time to study up on your Dune lore!`;
+    } else if (score < 8) {
+        displayResults.innerHTML = `You scored ${score} out of 10. You really know your way around the Imperium!`
+    } else {
+        displayResults.innerHTML = `You scored ${score} out of 10. Nice job! You're practically a mentat!`
+    }
+}
+
+function newGame () {
+    const questions = [question1, question2];
+    let score = 0;
+    let questions_asked = 0;
+    let currentQuestion = questions.pop();
+    nextQuestion();
+}
